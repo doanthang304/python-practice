57584713441802bd8b1822b63a8e68992c1db4af
doanthang <doanthang304204@gmail.com>
2026-05-10 22:35:56 +0700

    reverse int fix thieu test case

--- challenge/reverse_integer.py
+++ challenge/reverse_integer.py
@@ -5,14 +5,12 @@ class Solution:
         x = abs(x) # Biến x thành giá trị luôn dương
 
         result = 0
-        
-        if result > 2*31 -1 or result < -2*31:
-            return 0
         while x > 0:
             last_digit = x % 10
             result = result * 10 + last_digit
             x = x // 10
+        if result > 2**31 -1 or result < -2**31:
+            return 0
         return result * sign  
-
         
 print(Solution().reverse(-123))
